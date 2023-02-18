bcc393d2f82c0da5804038f50b7c326c7483f4eb
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-02-18 22:34:36 +0300

    shows how global variables are declared

--- lists.py
+++ lists.py
@@ -3,3 +3,18 @@ x, y, z = fruits
 print(x)
 print(y)
 print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
