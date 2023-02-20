d2c132361cd95b8e9c47c769903bde9fcdeaff12
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-02-20 21:01:59 +0300

    Shows variable types

--- lists.py
+++ lists.py
@@ -28,3 +28,27 @@ myfunc()
 
 print("Python is " + x)
 
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
---
+++ variables_type.py
@@ -0,0 +1,67 @@
+x = 5
+print(type(x)) 
+
+x = "Hello World"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = float(20.5)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = complex(1j)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = list(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+x = tuple(("apple", "banana", "cherry"))
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = range(6)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+x = dict(name="John", age=36)
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
