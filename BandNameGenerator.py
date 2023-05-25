111847639022a0233afa7872c7154e2340038a6d
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-05-25 22:28:19 +0300

    creates a program that generates names of a band

---
+++ BandNameGenerator.py
@@ -0,0 +1,9 @@
+#Create a welcome note for your program
+print("Welcome to the band name generator")
+# ask which city the user grew up in
+city = input("What city the user grew up in?")
+
+# ask the name of the user's pet
+pet = input("What is the name of your pet?")
+
+print("The name of your band could be" + city + "" + pet)
