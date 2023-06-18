8e884ed642fb1f66ee413e6f132f0d35680708c7
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-06-18 19:48:40 +0300

    shows how to use if else statements

--- if_else.py
+++ if_else.py
@@ -3,3 +3,12 @@ if water_level > 80:
     print("Drain Water")
 else:
     print("Continue")
+
+print("Welcome to the rollercoaster!")
+height= int(input("What is your height in cm?"))
+
+if height > 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry, you have to grow taller before you can ride")
+    
