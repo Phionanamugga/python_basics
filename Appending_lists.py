7ab43173105bfc73031c87e86d9747b4cae882eb
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-07-11 20:49:51 +0300

    shows where out of range error arises

--- Appending_lists.py
+++ Appending_lists.py
@@ -1,6 +1,7 @@
 fruits = ["Cherry", "Apple", "Pear"]
-print(fruits[0])
-fruits[0] = "Blue_Cherry"
-print(fruits)
-fruits.append("Kiwi")
-print(fruits)
+# print(fruits[0])
+# fruits[0] = "Blue_Cherry"
+# print(fruits)
+# fruits.append("Kiwi")
+# print(fruits)
+print(fruits[3])
